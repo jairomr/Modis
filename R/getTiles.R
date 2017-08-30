f32935fe91f4de039c5fddb66b8bb2c681aac415
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2017-08-30 15:40:39 -0300

    Test
    add pasta de teste

--- R/getTiles.R
+++ R/getTiles.R
@@ -43,4 +43,4 @@ getTiles<-function(latA,longA,latB,longB,latC,longC){
   return(return)
 }
 
-getTiles(latA=0,longA=-100,latB = 0,longB=-60,latC = -40,longC=-100)
+
---
+++ Teste/1EX.R
@@ -0,0 +1,1 @@
+tiles=getTiles(latA=0,longA=-100,latB = 0,longB=-60,latC = -40,longC=-100)
